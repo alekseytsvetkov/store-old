683328c4ddbb5e5b2a574252bb349f1c7e31def2
alekseytsvetkov <code.aleksey@icloud.com>
2023-09-01 11:43:21 +0300

    migrate to turbo

---
+++ apps/web/global.d.ts
@@ -0,0 +1,18 @@
+declare module '*.md' {
+  const text: string;
+  export default text;
+}
+declare module '*.png';
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production' | 'test';
+    GITHUB_ID: string;
+    GITHUB_SECRET: string;
+    DISCORD_CLIENT_ID: string;
+    DISCORD_CLIENT_SECRET: string;
+    PORT: number;
+    RESEND_API_KEY: string;
+    EDGE_CONFIG: string;
+  }
+}
